0877428f72d29e17456132ce725e54920a2f5e9d
sanjeebilly05 <Sanjeeb.Gurung@city.ac.uk>
2025-10-14 00:11:51 +0100

    New features
    Added text to add your code in, added a confirm feature to loop and make sure each part is correct and allowing you to retry. Added extra to the selenium, where you now access past the main menu of mcdonalds

--- mainPy.py
+++ mainPy.py
@@ -1,11 +1,44 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
-service = Service(executable_path="chromedriver.exe")
-driver = webdriver.Chrome(service=service)
+print("Welcome to McMan!\nUse this program to autocomplete your survey, and get your offer!")
+print("Don't worry about capital letters, we have that covered! :)")
+while True:
+    part1 = input("Enter your first code: ").strip().upper()
+    part2 = input("Enter your second code: ").strip().upper()
+    part3 = input("Enter your third code: ").strip().upper()
 
-driver.get("https://google.com")
+    if len(part1) == len(part2) == len(part3) == 4:
+        print("Your code is " + " - ".join([part1, part2, part3]))
+        while True:
+            confirm = input("Is this code correct? (y/n): ").strip().lower()
+            if confirm == "y":
+                print("Right! Let's continue on!")
+                break
+            elif confirm == "n":
+                print("Let's start again!")
+                break
+            else:
+                print("Invalid input, try again")
+            
+        if confirm == "y":
+            break
+    else:
+        print("Your code isn't right, please try again")
+
+print(" - ".join([part1, part2, part3]))
+
+service = Service(executable_path="chromedriver.exe")
+driver = webdriver.Chrome(service = service)
+driver.get("https://www.mcdfoodforthoughts.com/")
+
+time.sleep(10)
+
+driver.find_element(By.XPATH, "//input[@id = 'NextButton']").click()
 
 time.sleep(10)
 
