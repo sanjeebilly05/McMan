9df01e13fc3461af059b48091f79b944ab1cdc5e
sanjeebilly05 <Sanjeeb.Gurung@city.ac.uk>
2025-10-16 02:56:51 +0100

    updated the main functions PROBLEM
    doeznt seem to work, the id dont seem to connext with the thing im representing, perhaps  the fact i needed to expand the id was thje issue

--- mainPy.py
+++ mainPy.py
@@ -14,23 +14,43 @@ with open("stuff.txt", "r") as f:
 
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service = service)
+
 driver.get("https://www.mcdfoodforthoughts.com/")
 
-code2_id = data["fill_in_boxes"]["code2"]["id"]
-print(code2_id)
+# def wait_for_element(driver, by, value, timeout=5):
+#     try: 
+#         return WebDriverWait(driver, timeout).until(
+#             EC.presence_of_element_located((by, value))
+#     )
+#     except:
+#         return None
 
-price_high_id = data["options"]["price_high"]["id"]
-print(price_high_id)
+while True:
+    time.sleep(10)
+    for box in data["fill_in_boxes"].values():
+        try:
+            element = driver.find_element(By.ID, box["id"])
+            if "default_message" in box:
+                element.send_keys(box["default_message"])
+        except:
+            print(f"{box["id"]} not found")
 
-next_button = data["buttons"]["next_button"]["id"]
-print(next_button)
+    for option in data["options"].values():
+        try:
+            element = driver.find_element(By.ID, option["id"])
+            element.click()
+        except:
+            print(f"{option["id"]} not found")
 
-# def search_fill_in_box():
+    next_button_id = data["buttons"]["next_button"]["id"]
+    try:
+        next_button = driver.find_element(By.ID, next_button_id)
+        next_button.click()
+    except:
+        print("I'm done")
+        break
 
-# def search_options():
-
-# def search_button():
-# waitTime = random.randint(1, 2)
+driver.quit()
 
 # print("Welcome to McMan!\nUse this program to autocomplete your survey, and get your offer!")
 # print("Don't worry about capital letters, we have that covered! :)")
@@ -60,85 +80,3 @@ print(next_button)
 #         print("Your code or price isn't right, please try again")
 
 # # print(" - ".join([part1, part2, part3]))
-
-# part1 = "CLXW"
-# part2 = "G7LM"
-# part3 = "ZTTP"
-# pounds = 4
-# pence = 59
-
-# time.sleep(waitTime)
-
-# # Continue
-# try:
-#     main = WebDriverWait(driver, 10).until(
-#         EC.presence_of_element_located((By.ID, "NextButton"))
-#     )
-#     driver.find_element(By.ID, "NextButton").click()
-# except:
-#     driver.quit()
-
-# time.sleep(waitTime)
-
-# # Fill in code 1
-# code1 = driver.find_element(By.ID, "CN1")
-# code1.send_keys(part1)
-# time.sleep(waitTime)
-
-# # Fill in code 2
-# code2 = driver.find_element(By.ID, "CN2")
-# code2.send_keys(part2)
-# time.sleep(waitTime)
-
-# # Fill in code 3
-# code3 = driver.find_element(By.ID, "CN3")
-# code3.send_keys(part3)
-# time.sleep(waitTime)
-
-# # Fill in pounds
-# poundBox = driver.find_element(By.ID, "AmountSpent1")
-# poundBox.send_keys(pounds)
-# time.sleep(waitTime)
-
-# # Fill in pence
-# penceBox = driver.find_element(By.ID, "AmountSpent2")
-# penceBox.send_keys(pence)
-# time.sleep(waitTime)
-
-# # Start
-# driver.find_element(By.ID, "NextButton").click() 
-# time.sleep(waitTime)
-
-# #Take Away
-# driver.find_element(By.CLASS_NAME, "radioSimpleInput").click()
-# time.sleep(waitTime)
-
-# # Next
-# driver.find_element(By.ID, "NextButton").click()
-# time.sleep(waitTime)
-
-# # Highly Satisfied
-# driver.find_element(By.CLASS_NAME, "Opt5")
-# time.sleep(waitTime)
-
-# # Next
-# driver.find_element(By.ID, "NextButton").click() 
-# time.sleep(waitTime)
-
-# # <label for="R000002.5" class="radioSimpleInput">‍</label>
-# # The ease of placing your order
-# driver.find_element(By.ID, "R000016.5").click()
-
-# # The quality of your food and drink
-# driver.find_element(By.ID, "R000008.5").click()
-
-# # How well your order was packaged
-# driver.find_element(By.ID, "R000144.5").click()
-
-# # The friendliness of the staff
-# driver.find_element(By.ID, "R000019.5").click()
-
-# # The temperature of your food or drink
-# driver.find_element(By.ID, "R000010.5").click()
-
-# driver.quit()
