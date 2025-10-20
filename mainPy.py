8dee787dde0671caad294a40c1fdf1434fd17698
sanjeebilly05 <Sanjeeb.Gurung@city.ac.uk>
2025-10-20 01:28:06 +0100

    finished code
    as of right now, the code is finished. just needs to test the code with the new info

--- mainPy.py
+++ mainPy.py
@@ -11,72 +11,73 @@ import json
 with open("stuff.txt", "r") as f:
     data = json.load(f)
 
+print("Welcome to McMan!\nUse this program to autocomplete your survey, and get your offer!")
+print("Don't worry about capital letters, we have that covered! :)")
+
+while True:
+    pounds = input("Enter the pounds of the meal: ").strip()
+    data["fill_in_boxes"]["pounds"]["default_message"] = pounds
+
+    pence = input("Enter the pence of your meal: ").strip()
+    data["fill_in_boxes"]["pence"]["default_message"] = pence
+
+    part1 = input("Enter your first code: ").strip().upper()
+    data["fill_in_boxes"]["code1"]["default_message"] = part1
+
+    part2 = input("Enter your second code: ").strip().upper()
+    data["fill_in_boxes"]["code2"]["default_message"] = part2
+
+    part3 = input("Enter your third code: ").strip().upper()
+    data["fill_in_boxes"]["code3"]["default_message"] = part3
+
+
+    if len(pounds) < 4 and len(pence) < 3 and len(part1) == len(part2) == len(part3) == 4:
+        print("Your code is " + " - ".join([part1, part2, part3]) + " and you have spent " + "£" + pounds + "." + pence)
+        while True:
+            confirm = input("Is this code correct? (y/n): ").strip().lower()
+            if confirm == "y":
+                print("Right! Let's continue on!")
+                break
+            elif confirm == "n":
+                print("Let's start again!")
+                break
+            else:
+                print("Invalid input, try again")
+            
+        if confirm == "y":
+            break
+    else:
+        print("Your code or price isn't right, please try again")
+
+print(" - ".join([part1, part2, part3]))
 
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service = service)
-
 driver.get("https://www.mcdfoodforthoughts.com/")
-
-# def wait_for_element(driver, by, value, timeout=5):
-#     try: 
-#         return WebDriverWait(driver, timeout).until(
-#             EC.presence_of_element_located((by, value))
-#     )
-#     except:
-#         return None
 
 while True:
     time.sleep(10)
     for box in data["fill_in_boxes"].values():
         try:
-            element = driver.find_element(By.ID, box["id"])
+            element = driver.find_element(By.XPATH, box["XPath"])
             if "default_message" in box:
                 element.send_keys(box["default_message"])
         except:
-            print(f"{box["id"]} not found")
+            print(f"{box["XPath"]} not found")
 
     for option in data["options"].values():
         try:
-            element = driver.find_element(By.ID, option["id"])
+            element = driver.find_element(By.XPATH, option["XPath"])
             element.click()
         except:
-            print(f"{option["id"]} not found")
+            print(f"{option["XPath"]} not found")
 
-    next_button_id = data["buttons"]["next_button"]["id"]
+    next_button_XPath = data["buttons"]["next_button"]["XPath"]
     try:
-        next_button = driver.find_element(By.ID, next_button_id)
+        next_button = driver.find_element(By.XPATH, next_button_XPath)
         next_button.click()
     except:
         print("I'm done")
         break
 
 driver.quit()
-
-# print("Welcome to McMan!\nUse this program to autocomplete your survey, and get your offer!")
-# print("Don't worry about capital letters, we have that covered! :)")
-# while True:
-#     pounds = input("Enter the pounds of the meal: ").strip()
-#     pence = input("Enter the pence of your meal: ").strip()
-#     part1 = input("Enter your first code: ").strip().upper()
-#     part2 = input("Enter your second code: ").strip().upper()
-#     part3 = input("Enter your third code: ").strip().upper()
-
-#     if len(pounds) < 4 and len(pence) < 3 and len(part1) == len(part2) == len(part3) == 4:
-#         print("Your code is " + " - ".join([part1, part2, part3]) + " and you have spent " + "£" + pounds + "." + pence)
-#         while True:
-#             confirm = input("Is this code correct? (y/n): ").strip().lower()
-#             if confirm == "y":
-#                 print("Right! Let's continue on!")
-#                 break
-#             elif confirm == "n":
-#                 print("Let's start again!")
-#                 break
-#             else:
-#                 print("Invalid input, try again")
-            
-#         if confirm == "y":
-#             break
-#     else:
-#         print("Your code or price isn't right, please try again")
-
-# # print(" - ".join([part1, part2, part3]))
